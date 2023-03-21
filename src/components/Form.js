b036ed8143b362df11e2ea6467331c74a49f0811
Maria Cosmen <mcosme00@gmail.com>
2023-03-21 23:07:54 +0900

    Add external data file

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import data from '../meme_data';
 
 const Form = () => {
+  const result = React.useState("Hello")
+  console.log(result);
+  console.log(data)
+  const memes = data.data.memes;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(memes[Math.floor(Math.random() * memes.length)].url)
+  }
+
   return (
     <form>
       <div className='input-container'>
-        <input type="text" placeholder="Some text"></input>
-        <input type="text" placeholder="Some text"></input>
+        <input type="text" placeholder="Some text" autofocus={true}></input>
+        <input type="text" placeholder="Some text" autofocus={true}></input>
       </div>
-      <input type="submit" value="Search meme" className="button input-btn"></input>
+      <button onClick={handleSubmit} type="submit" className="button input-btn">Search meme</button>
     </form>
   )
 };
