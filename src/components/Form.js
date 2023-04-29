7971feb41489f8a1e05b21e34d58e7afdce03749
Maria Cosmen <mcosme00@gmail.com>
2023-04-29 23:23:53 +0900

    Add state to change images

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,24 +1,29 @@
-import React from 'react';
-import data from '../meme_data';
+import React, { useState } from 'react';
+import data from '../meme_data.js';
 
 const Form = () => {
-  const result = React.useState("Hello")
-  console.log(result);
-  console.log(data)
+  const [memeImage, setMemeImage] = useState("https://i.imgflip.com/46hhvr.jpg")
   const memes = data.data.memes;
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(memes[Math.floor(Math.random() * memes.length)].url)
+    const randomNumber = Math.floor(Math.random() * memes.length)
+    const randomMeme = memes[randomNumber].url
+    console.log(randomMeme)
+    setMemeImage(randomMeme)
   }
 
   return (
-    <form>
-      <div className='input-container'>
-        <input type="text" placeholder="Some text" autofocus={true}></input>
-        <input type="text" placeholder="Some text" autofocus={true}></input>
-      </div>
-      <button onClick={handleSubmit} type="submit" className="button input-btn">Search meme</button>
-    </form>
+    <div>
+      <form>
+        <div className='input-container'>
+          <input type="text" placeholder="Some text" autofocus={true}></input>
+          <input type="text" placeholder="Some text" autofocus={true}></input>
+        </div>
+        <button onClick={handleSubmit} type="submit" className="button input-btn">Search meme</button>
+      </form>
+      <img src={memeImage} alt="random meme" className="meme-image"/>
+    </div>
   )
 };
 
