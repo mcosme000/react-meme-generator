f47ea352fdaafd53f3ea98bc4cc43200a0df4875
Maria Cosmen <mcosme00@gmail.com>
2023-05-07 23:03:34 +0900

    Change container padding

--- src/components/Form.js
+++ src/components/Form.js
@@ -14,29 +14,27 @@ const Form = ({onSubmit, memeImage, setMemeImage }) => {
   }
 
   return (
-    <div>
-      <form onSubmit={onSubmit}>
-        <div className='input-container'>
-          <input
-            type="text"
-            placeholder="Add some top text"
-            autofocus={true}
-            name="topText"
-            value={memeImage.topText}
-            onChange={handleChange}>
-          </input>
-          <input
-            type="text"
-            placeholder="Add some bottom text"
-            autofocus={true}
-            name="bottomText"
-            value={memeImage.bottomText}
-            onChange={handleChange}>
-          </input>
-        </div>
-        <button onClick={onSubmit} className="button input-btn wide-btn">Get random meme</button>
-      </form>
-    </div>
+    <form onSubmit={onSubmit}>
+      <div className='input-container'>
+        <input
+          type="text"
+          placeholder="Add some top text"
+          autofocus={true}
+          name="topText"
+          value={memeImage.topText}
+          onChange={handleChange}>
+        </input>
+        <input
+          type="text"
+          placeholder="Add some bottom text"
+          autofocus={true}
+          name="bottomText"
+          value={memeImage.bottomText}
+          onChange={handleChange}>
+        </input>
+      </div>
+      <button onClick={onSubmit} className="button input-btn wide-btn">Get random meme</button>
+    </form>
   )
 };
 
